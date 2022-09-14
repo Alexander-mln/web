2eb28e0fcc84f38010f0ca71cfcfe87aefe4e487
ubu20 <ubu20@ubu20>
2022-09-14 21:32:47 +0300

    update

--- hello.py
+++ hello.py
@@ -2,8 +2,9 @@
 
 def app(environ, start_response):
     data = ""
-    for i in (environ.split("&")):
-        data += f'{i.replace("/", "").replace("?", "")}\n'
+    for i in (environ['QUERY_STRING'].split("&")):
+        data += i + "\n"
+    # data = bytes(data, 'utf-8')
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
