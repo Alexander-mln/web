3af82c5105faa53bbbfdde56531ef9462a951f90
ubu20 <ubu20@ubu20>
2022-09-14 20:17:12 +0300

    update

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 
 
 def app(environ, start_response):
-    data = b"Hello, World!\n "
+    data = ""
+    for i in (environ.split("&")):
+        data += f'{i.replace("/", "").replace("?", "")}\n'
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
